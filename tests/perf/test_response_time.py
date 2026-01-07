b4ab75b5cbd130b589761869d267dc2b65430da5
konrad <konrad@tyk.io>
2026-01-07 10:48:03 +0100

    update

--- tests/perf/test_response_time.py
+++ tests/perf/test_response_time.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 import requests
 
 class PerfTest(unittest.TestCase):
@@ -10,12 +11,31 @@ class PerfTest(unittest.TestCase):
     url = "http://localhost:5000/api/accounts"
     iteration_count = 100
     timeout = 0.5
+    
+    @pytest.fixture(autouse=True, scope="function")
+    def clear(self):
+        response = requests.get(self.url, timeout=self.timeout)
+        accounts = response.json()
+        for account in accounts:
+            pesel = account["pesel"]
+            requests.delete(f"{self.url}/{pesel}", timeout=self.timeout)
+
 
     def test_create_delete_perf_test(self):
         for i in range(self.iteration_count):
             create_response = requests.post(self.url, json=self.body, timeout=self.timeout)
             assert create_response.status_code == 201
             delete_respone = requests.delete(f"{self.url}/{self.body['pesel']}", timeout=self.timeout)
+            assert delete_respone.status_code == 200
+            
+    def test_create_delete_perf_group(self):
+        pesels = [f"12345678{i:03d}" for i in range(1000)]
+        for pesel in pesels:
+            body = {**self.body, "pesel": pesel}
+            create_response = requests.post(self.url, json=body, timeout=self.timeout)
+            assert create_response.status_code == 201
+        for pesel in pesels:
+            delete_respone = requests.delete(f"{self.url}/{pesel}", timeout=self.timeout)
             assert delete_respone.status_code == 200
             
     def test_transfer_perf(self):
