35ea9d878623e1b353505b0ebd013c6cbe68ae52
konrad <konrad@tyk.io>
2026-01-07 10:27:57 +0100

    update

---
+++ tests/perf/test_response_time.py
@@ -0,0 +1,33 @@
+import unittest
+import requests
+
+class PerfTest(unittest.TestCase):
+    body = {
+        "name" : "Dariusz",
+        "surname": "Januszewski",
+        "pesel":"12345678901"
+    }
+    url = "http://localhost:5000/api/accounts"
+    iteration_count = 100
+    timeout = 0.5
+
+    def test_create_delete_perf_test(self):
+        for i in range(self.iteration_count):
+            create_response = requests.post(self.url, json=self.body, timeout=self.timeout)
+            assert create_response.status_code == 201
+            delete_respone = requests.delete(f"{self.url}/{self.body['pesel']}", timeout=self.timeout)
+            assert delete_respone.status_code == 200
+            
+    def test_transfer_perf(self):
+        create_response = requests.post(self.url, json=self.body, timeout=1)
+        assert create_response.status_code == 201
+        for i in range(self.iteration_count):
+            transfer_response = requests.post(f"{self.url}/{self.body['pesel']}/transfer",
+                                              json={"type": "incoming", "amount": 100}, timeout=self.timeout)
+            assert transfer_response.status_code == 200
+        account = requests.get(f"{self.url}/{self.body['pesel']}", timeout=self.timeout)
+        print(account.json())
+        assert account.json()["balance"] == 100*self.iteration_count
+        delete_respone = requests.delete(f"{self.url}/{self.body['pesel']}", timeout=self.timeout)
+        assert delete_respone.status_code == 200
+           
